fed5a3f303161591b218d0842dabc1457ccc31b9
FineArchs <cicada1003@gmail.com>
2026-09-09 22:22:06 +0900

    CIのplaygroundビルドが落ちるので本体への参照を修正

--- playground/src/App.vue
+++ playground/src/App.vue
@@ -52,8 +52,8 @@
 
 <script setup>
 import { ref, watch } from 'vue';
-import { AISCRIPT_VERSION, Interpreter, Parser, utils } from '@finearchs/faiscript';
-import { std } from '@finearchs/faiscript/interpreter/lib/std';
+import { AISCRIPT_VERSION, Interpreter, Parser, utils } from '../../built/index.js';
+import { std } from '../../built/interpreter/lib/std.js';
 
 import { PrismEditor } from 'vue-prism-editor';
 import 'vue-prism-editor/dist/prismeditor.min.css';
